f799dc786dab100d96c8bc105c7ead31930dddb6
Isiah Obera <ijobera@springvalley.tech>
2018-06-29 16:45:43 +0800

    Category Page

--- blog/app/Http/Controllers/FrontEndController.php
+++ blog/app/Http/Controllers/FrontEndController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Tag;
 use App\Post;
 use App\Category;
 use App\Setting;
@@ -32,8 +33,19 @@ class FrontEndController extends Controller
         return view('single')->with('post', $post)
                              ->with('title', $post->title)
                              ->with('settings', Setting::first())
-                             ->with('categories', Category::take(4)->get())
+                             ->with('categories', Category::take(5)->get())
                              ->with('next', Post::find($next_id))
-                             ->with('prev', Post::find($prev_id));
+                             ->with('prev', Post::find($prev_id))
+                             ->with('tags', Tag::all());
+    }
+
+    public function category($id)
+    {
+        $category = Category::find($id);
+
+        return view('category')->with('category', $category)
+                               ->with('title', $category->name)
+                               ->with('settings', Setting::first())
+                               ->with('categories', Category::take(5)->get());
     }
 }
--- blog/resources/views/single.blade.php
+++ blog/resources/views/single.blade.php
@@ -44,7 +44,7 @@
         
                                     <span class="category">
                                         <i class="seoicon-tags"></i>
-                                        <a href="#">{{ $post->category->name }}</a>
+                                        <a href="{{ route('category.single', ['id' => $post->category->id]) }}">{{ $post->category->name }}</a>
                                     </span>
         
                                 </div>
@@ -158,10 +158,8 @@
                                     <span class="long-line"></span>
                                 </div>
                             </div>
-                        </div>
-        
-                        <div class="row">
-        
+
+                            @include('includes.disqus')
                         </div>
         
         
@@ -170,6 +168,9 @@
                     <!-- End Post Details -->
         
                     <!-- Sidebar-->
+                    
+                    <br>
+                    <br>
         
                     <div class="col-lg-12">
                         <aside aria-label="sidebar" class="sidebar sidebar-right">
@@ -183,15 +184,9 @@
                                 </div>
         
                                 <div class="tags-wrap">
-                                    <a href="#" class="w-tags-item">SEO</a>
-                                    <a href="#" class="w-tags-item">Advertising</a>
-                                    <a href="#" class="w-tags-item">Business</a>
-                                    <a href="#" class="w-tags-item">Optimization</a>
-                                    <a href="#" class="w-tags-item">Digital Marketing</a>
-                                    <a href="#" class="w-tags-item">Social</a>
-                                    <a href="#" class="w-tags-item">Keyword</a>
-                                    <a href="#" class="w-tags-item">Strategy</a>
-                                    <a href="#" class="w-tags-item">Audience</a>
+                                    @foreach($tags as $tag)
+                                        <a href="#" class="w-tags-item">{{ $tag->tag }}</a>
+                                    @endforeach
                                 </div>
                             </div>
                         </aside>
