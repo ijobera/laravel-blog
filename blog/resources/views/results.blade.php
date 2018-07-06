99a1e9cf95a556f1fb04c151874ca870acf5d626
Isiah Obera <ijobera@springvalley.tech>
2018-07-06 11:12:16 +0800

    Searching post title

--- blog/resources/views/results.blade.php
+++ blog/resources/views/results.blade.php
@@ -1,20 +1,21 @@
-    @extends('layouts.frontend')
+@extends('layouts.frontend')
 
-    @section('content')
+@section('content')
 
-        <div class="stunning-header stunning-header-bg-lightviolet">
-            <div class="stunning-header-content">
-                <h1 class="stunning-header-title">Category: {{ $category->name }}</h1>
-            </div>
+    <div class="stunning-header stunning-header-bg-lightviolet">
+        <div class="stunning-header-content">
+            <h1 class="stunning-header-title">Search results: {{ $query }}</h1>
         </div>
+    </div>
 
-        <div class="container">
-            <div class="row medium-padding120">
-                <main class="main">
+    <div class="container">
+        <div class="row medium-padding120">
+            <main class="main">
 
-                    <div class="row">
+                <div class="row">
+                    @if($posts->count() > 0)
                         <div class="case-item-wrap">
-                            @foreach($category->posts as $post)
+                            @foreach($posts as $post)
                                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                                     <div class="case-item">
                                         <div class="case-item__thumb">
@@ -25,9 +26,12 @@
                                 </div>
                             @endforeach
                         </div>
-                    </div>
-                </main>
-            </div>
+                    @else
+                        <h1 class="text-center">No results found.</h1>
+                    @endif
+                </div>
+            </main>
         </div>
+    </div>
 
-    @endsection
+@endsection
